296de1e1428576f2dd55855db417c4c6a72ae9a1
天玮 张 <twzhang1124@163.com>
2022-12-19 20:18:48 +0800

    set web

--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">未来的吃鸡游戏</h1>'
+    line2 = '<img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Flmg.jj20.com%2Fup%2Fallimg%2F4k%2Fs%2F01%2F21092412092K1E-0-lp.jpg&refer=http%3A%2F%2Flmg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1674043970&t=487a3f780883011ee5e1a303f7f8e423" width = 1500>'
+    return HttpResponse(line1+line2)
+
